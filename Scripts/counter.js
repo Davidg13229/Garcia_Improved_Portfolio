6886827efefb43b24ea5ffdded4f815eb8c9ff50
David Garcia <davidg13229@gmail.com>
2026-04-01 19:10:34 +0800

    Updates to html

--- Scripts/counter.js
+++ Scripts/counter.js
@@ -15,3 +15,4 @@ const KEY = "david-personal-portfolio";
 // Paste This in browser: https://countapi.mileshilliard.com/api/v1/set/your_key?value=100
 // Change the "your_key" to the const KEY (david-personal-portfolio)
 // set the desired value from 100 to 0 
+
